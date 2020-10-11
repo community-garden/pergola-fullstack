78709998a5bf1efab249ff4215ec850b15c4fcb1
bastiion <graviola@posteo.de>
2020-10-11 23:09:17 +0200

    project package.json and generic build scripts

---
+++ scripts/build.js
@@ -0,0 +1,20 @@
+const concurrently = require('concurrently')
+
+const { API_DIR, APP_DIR, runner, concurrentOpts } = require('./common')
+
+const jobs = [
+  {
+    name: 'api',
+    command: `cd ${API_DIR} && ${runner} run build`,
+    prefixColor: 'green',
+  },
+  {
+    name: 'app',
+    command: `cd ${APP_DIR} && ${runner} run build`,
+    prefixColor: 'blue',
+  },
+]
+
+concurrently(jobs, concurrentOpts).catch((e) => {
+  console.error(e.message)
+})
---
+++ scripts/common.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const execa = require('execa')
+
+const API_DIR = path.join(__dirname, '../api')
+const APP_DIR = path.join(__dirname, `../app`)
+
+const shouldUseYarn = () => {
+  try {
+    execa.sync('yarnpkg', ['--version'])
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+const runner = shouldUseYarn() ? 'yarn' : 'npm'
+
+const concurrentOpts = {
+  restartTries: 3,
+  prefix: '{time} {name} |',
+  timestampFormat: 'HH:mm:ss',
+}
+
+module.exports = {
+  concurrentOpts,
+  runner,
+  API_DIR,
+  APP_DIR,
+}
