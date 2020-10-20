5f83a07171274d284f4bd560fabd9a83d84f7cec
bastiion <graviola@posteo.de>
2020-10-20 17:01:09 +0200

    tslinter, backend ported to typescript, app firectory removed

--- api/src/initialize.ts
+++ api/src/initialize.ts
@@ -1,9 +1,10 @@
+import { Driver } from 'neo4j-driver'
 import { logger } from './index'
 
 export const initializeDatabase = (driver: Driver) => {
   const initCypher = `CALL apoc.schema.assert({}, {User: ["userId"], Business: ["businessId"], Review: ["reviewId"], Category: ["name"]})`
 
-  const executeQuery = (driver) => {
+  const executeQuery = (_driver: Driver) => {
     const session = driver.session()
     return session
       .writeTransaction((tx) => tx.run(initCypher))
