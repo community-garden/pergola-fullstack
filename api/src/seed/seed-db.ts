b20673f1031ecda8488ef26d76ce76cd364f963a
bastiion <graviola@posteo.de>
2021-01-19 22:06:38 +0100

    fining seeding script

--- api/src/seed/seed-db.ts
+++ api/src/seed/seed-db.ts
@@ -22,12 +22,7 @@ const logger = pino( {
 
 dotenv.config()
 
-// Specify host, port and path for GraphQL endpoint
-const port = process.env.GRAPHQL_SERVER_PORT || 4001
-const path = process.env.GRAPHQL_SERVER_PATH || "/graphql"
-const host = process.env.GRAPHQL_SERVER_HOST || "0.0.0.0"
-
-const uri = `http://${host}:${port}${path}`
+const uri = process.env.GRAPHQL_SERVER_URL || "http://localhost:4001/graphql"
 
 const runMutations = async () => {
   const token = await getToken()
