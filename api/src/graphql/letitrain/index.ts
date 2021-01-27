8aa327b801a6d2934bae066565502ab84fcb1594
bastiion <graviola@posteo.de>
2021-01-27 19:12:25 +0100

    frim assignableWateringTimeWindow to assignableWateringPeriod

--- api/src/graphql/letitrain/index.ts
+++ api/src/graphql/letitrain/index.ts
@@ -10,5 +10,7 @@ export default {
       ...planWateringPeriods.resolvers.Mutation,
     },
   },
-  schema: { mutation: [[__dirname, "mutation.schema.graphql"]], query: [[__dirname, "query.schema.graphql"]] },
+  schema: {
+    mutation: [[__dirname, "mutation.schema.graphql"]],
+    query: [[__dirname, "query.schema.graphql"]] },
 }
