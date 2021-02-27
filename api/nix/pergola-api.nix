3f0c53203782b6d9fea1d7162e0c821f8b3055e8
Johannes Lötzsch <git@johannesloetzsch.de>
2021-02-27 23:53:45 +0100

    startup script

--- api/nix/pergola-api.nix
+++ api/nix/pergola-api.nix
@@ -5,23 +5,34 @@
 
 let
   nodeDependencies = (pkgs.callPackage ./deps/default.nix {}).shell.nodeDependencies;
-in
-stdenv.mkDerivation {
-  name = "pergola-api";
-  src = ./..;
-  buildInputs = [ nodejs ] ++ (with pkgs; [ which ]);
-  buildPhase = ''
-    ln -s ${nodeDependencies}/lib/node_modules ./node_modules
-    export PATH="${nodeDependencies}/bin:$PATH"
 
-    #npm run build-ts
-    [ -d dist ] || mkdir dist
-    npm run prepare-build
-    node --version
-    tsc
-  '';
-  installPhase = ''
-    mkdir $out
-    cp -r dist $out/
-  '';
-}
+  pergolaApiNode = stdenv.mkDerivation {
+    name = "pergola-api-node";
+    src = ./..;
+    buildInputs = [ nodejs ] ++ (with pkgs; [ which ]);
+    buildPhase = ''
+      ln -s ${nodeDependencies}/lib/node_modules ./node_modules
+      export PATH="${nodeDependencies}/bin:$PATH"
+
+      #npm run build-ts
+      [ -d dist ] || mkdir dist
+      npm run prepare-build
+      node --version
+      tsc
+    '';
+    installPhase = ''
+      mkdir $out
+      cp -r dist $out/
+    '';
+  };
+in
+(pkgs.writeScriptBin "pergola-api" ''
+  #!${pkgs.runtimeShell} -xe
+
+  ln -s ${nodeDependencies}/lib/node_modules ./node_modules
+  export PATH="${nodeDependencies}/bin:$PATH"
+
+  export KEYCLOAK_CONFIG="''${KEYCLOAK_CONFIG:-"config/keycloak.json"}"
+  ts-node ${pergolaApiNode}/dist/index.js
+  ''
+)
